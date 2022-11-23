11eb7ee8e35135785f6b5e2233b95282d62d85fa
OlegStankevich1973 <stankeviz@mail.ru>
2022-11-23 14:55:55 +0300

    Программа вывода даты и времени

---
+++ task002.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+// Вывести в консоль системные дату и время
+
+public class task002 {
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
