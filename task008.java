f7129c5017e82f795a9ad06c5181becb000b6146
OlegStankevich1973 <stankeviz@mail.ru>
2022-11-29 01:21:48 +0300

    Простейший калькулятор

---
+++ task008.java
@@ -0,0 +1,62 @@
+// ================================================================================
+// Реализовать простой калькулятор
+// ==================================================================================
+
+import java.util.Scanner;
+
+public class task008 {
+    public static void main(String[] args) {
+        Calc myObject = new Calc();
+        myObject.calcTask();
+        
+    }
+}
+
+class Calc {// не делать одинаковые названия классов а то происходит почему то вытаскивание решения из другой задачи
+    double numberOne;
+    double numberTwo;
+    double result;
+    char action;//вводится один символ метод char
+    
+    Calc() {//создаем конструктор
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите первое число ");
+        numberOne = input.nextInt();//вводим первое число в консоль
+        System.out.println("Введите действие (+,-,/,*)");
+        action = input.next().charAt(0);//индекс 0 тк вводится умножение или сложение одним символом с индексом 0
+        System.out.println("Введите второе число ");
+        numberTwo = input.nextInt();//вводим второе число в консоль
+
+        input.close();
+    }
+
+    void calcTask() {        
+        switch (action) {
+            case '+':
+                result = numberOne + numberTwo;
+                break;
+            case '-':
+                result = numberOne - numberTwo;
+                break;
+            case '*':
+                result = numberOne * numberTwo;
+                break;
+            case '/':
+                result = numberOne / numberTwo;
+                break;
+            default:
+                System.out.printf("Error! Enter correct operator");
+                return;
+            }
+        
+
+        // System.out.print("\nThe result is given as follows:\n");
+        System.out.printf(numberOne + " " + action + " " + numberTwo + " = " + result);
+           
+    }
+
+
+}
+
+    
+
