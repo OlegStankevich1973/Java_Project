34294402b9128b73dc1a2d9b6bb91206e2e551a5
OlegStankevich1973 <stankeviz@mail.ru>
2022-12-08 22:20:55 +0300

    Программа удаляет из коллекции четные числа

---
+++ DZ3Task1/task1.java
@@ -0,0 +1,65 @@
+package DZ3Task1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+/*
+ * Пусть дан произвольный список целых чисел, удалить из него четные числа
+*/
+
+public class task1 {
+    public static  void main(String[] args) {
+        TaskOne myObject = new TaskOne();
+        myObject.taskOne();
+    }
+}
+
+class TaskOne {
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    // метод, объединяющий
+    public void taskOne() {
+        System.out.println("Исходный список");
+        numbers = randomNumberList();
+        System.out.println(numbers);
+        System.out.print("\n");
+
+        System.out.println("Выходной список");
+        numbers = delEven();
+        System.out.println(numbers);
+
+    }
+
+    // метод генерации случайного числа
+    public int checkRandom() {
+        Random random = new Random();
+        int min = 1;
+        int max = 1000;
+        int diff = max - min;
+        int num = random.nextInt(diff + 1) + min;
+        return num;
+
+    }
+
+    // метод запонения ArrayList
+    public ArrayList<Integer> randomNumberList() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            numbers.add(checkRandom());
+        }
+        return numbers;
+    }
+
+    // Метод удаления четных чисел
+    public ArrayList<Integer> delEven() {
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            int temp = iterator.next();
+            if (temp % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        return numbers;
+    }
+
+}
